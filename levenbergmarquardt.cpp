8e80e978d39e1baf04aab52ed5a07569aaf5cef1
yanglinsensjtu <yanglinsensjtu@126.com>
2021-05-19 21:01:30 +0800

    add jocabian function

--- levenbergmarquardt.cpp
+++ levenbergmarquardt.cpp
@@ -1,6 +1,16 @@
 #include "levenbergmarquardt.h"
-
+#include "Eigen/Core"
+#include "fourparameterfunction.h"
 LevenbergMarquardt::LevenbergMarquardt()
 {
 
 }
+
+Eigen::MatrixX4d LevenbergMarquardt::jacobian(Eigen::MatrixXd J_A,
+                                              Eigen::MatrixXd J_B,
+                                              Eigen::MatrixXd J_C,
+                                              Eigen::MatrixXd J_D)
+{
+
+}
+
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,16 +2,21 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QVector>
+#include "Eigen/Core"
+#include <iostream>
+
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Determinant * D = new Determinant();
-    QVector<int> V = D->inivec(5);
-    QVector<QVector<int>> vec_seq;
-    D->Perm(V,&vec_seq,0,V.size()-1);
+//    Determinant * D = new Determinant();
+//    QVector<int> V = D->inivec(5);
+//    QVector<QVector<int>> vec_seq;
+//    D->Perm(V,&vec_seq,0,V.size()-1);
 //    double** array = new double*[2];
 //    for (int i = 0; i < 2; i++)
 //    {
@@ -22,16 +27,20 @@ MainWindow::MainWindow(QWidget *parent)
 //    array[1][0]=1.13445;
 //    array[1][1]=1.1394835;
 
-    QVector<QVector<double>> vec_determ;
-    QVector<double> vec1, vec2;
-    vec1.push_back(1.123);
-    vec1.push_back(12.34);
-    vec2.push_back(1.837);
-    vec2.push_back(2.3948);
-    vec_determ.push_back(vec1);
-    vec_determ.push_back(vec2);
+//    QVector<QVector<double>> vec_determ;
+//    QVector<double> vec1, vec2;
+//    vec1.push_back(1.123);
+//    vec1.push_back(12.34);
+//    vec2.push_back(1.837);
+//    vec2.push_back(2.3948);
+//    vec_determ.push_back(vec1);
+//    vec_determ.push_back(vec2);
 
-    qDebug()<<  vec_determ[0][0]<<D->calculate(vec_determ, 2);
+
+
+
+
+//    qDebug()<<  vec_determ[0][0]<<D->calculate(vec_determ, 2);
 
 
 
