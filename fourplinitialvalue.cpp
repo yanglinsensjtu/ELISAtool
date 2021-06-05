856ec0cdc1193f3bf8ae157a2755c278b52a92dc
yanglinsensjtu <yanglinsensjtu@126.com>
2021-06-05 22:28:38 +0800

    update

--- fourplinitialvalue.cpp
+++ fourplinitialvalue.cpp
@@ -31,15 +31,49 @@ void FourPLInitialValue::setInitialA(QVector<float> value)
 {
     initialA = *min_element(value.begin(),value.end());
 }
-//从检测数据的均值中返回最大值作为D的初值
 float FourPLInitialValue::getInitialD() const
 {
     return initialD;
 }
 
+//从检测数据的均值中返回最大值作为D的初值
 void FourPLInitialValue::setInitialD(QVector<float> value)
 {
     initialD = *max_element(value.begin(),value.end());
+}
+
+QVector<float> FourPLInitialValue::getVariationY() const
+{
+    return variationY;
+}
+
+void FourPLInitialValue::setVariationY(const QVector<float> &value, float initialA, float initialD)
+{
+    QVector<float> tmp1;
+    for(int i = 0; i <value.size(); i ++){
+        tmp1.push_back(value.at(i)-initialD);
+    }
+    QVector<float> tmp2;
+    for(int i = 0; i <value.size(); i ++){
+        tmp2.push_back(initialA-value.at(i));
+    }
+    QVector<float> tmp3;
+    for(int i = 0; i <value.size(); i ++){
+        tmp3.push_back(log(tmp1.at(i)/tmp2.at(i)));
+    }
+    variationY = tmp3;
+}
+
+
+
+QVector<float> FourPLInitialValue::getVariationX() const
+{
+    return variationX;
+}
+
+void FourPLInitialValue::setVariationX(const QVector<float> &value)
+{
+    variationX = value;
 }
 
 FourPLInitialValue::FourPLInitialValue()
