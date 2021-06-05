66207441670df3c9318ba6cb051c79b2349817bc
yanglinsensjtu <yanglinsensjtu@126.com>
2021-06-05 21:42:28 +0800

    求AD的初值

--- fourplinitialvalue.cpp
+++ fourplinitialvalue.cpp
@@ -1,6 +1,48 @@
 #include "fourplinitialvalue.h"
+#include <cmath>
+#include <algorithm>
+using namespace std;
+QVector<float> FourPLInitialValue::getReadValue() const
+{
+    return readValue;
+}
+
+void FourPLInitialValue::setReadValue(const QVector<float> &value)
+{
+    readValue = value;
+}
+
+QVector<float> FourPLInitialValue::getDilutionDrugValue() const
+{
+    return dilutionDrugValue;
+}
+
+void FourPLInitialValue::setDilutionDrugValue(const QVector<float> &value)
+{
+    dilutionDrugValue = value;
+}
+
+float FourPLInitialValue::getInitialA() const
+{
+    return initialA;
+}
+//从检测数据的均值中返回最小值作为A的初值
+void FourPLInitialValue::setInitialA(QVector<float> value)
+{
+    initialA = *min_element(value.begin(),value.end());
+}
+//从检测数据的均值中返回最大值作为D的初值
+float FourPLInitialValue::getInitialD() const
+{
+    return initialD;
+}
+
+void FourPLInitialValue::setInitialD(QVector<float> value)
+{
+    initialD = *max_element(value.begin(),value.end());
+}
 
 FourPLInitialValue::FourPLInitialValue()
 {
-
+    
 }
--- fourplinitialvalue.h
+++ fourplinitialvalue.h
@@ -1,11 +1,25 @@
 #ifndef FOURPLINITIALVALUE_H
 #define FOURPLINITIALVALUE_H
-
+#include <QVector>
 
 class FourPLInitialValue
 {
+private:
+    float initialA, initialB, initialC, initialD;
+    QVector<float> variationY, variationX;
+    QVector<float> readValue, dilutionDrugValue;
+
 public:
     FourPLInitialValue();
+    ~FourPLInitialValue();
+    QVector<float> getReadValue() const;
+    void setReadValue(const QVector<float> &value);
+    QVector<float> getDilutionDrugValue() const;
+    void setDilutionDrugValue(const QVector<float> &value);
+    float getInitialA() const;
+    void setInitialA(QVector<float> value);
+    float getInitialD() const;
+    void setInitialD(QVector<float> value);
 };
 
 #endif // FOURPLINITIALVALUE_H
