d2c62578f3aec09d3dea3574beee01290efa9227
yanglinsensjtu <yanglinsensjtu@126.com>
2021-06-06 00:16:18 +0800

    求解二元一次方程组的矩阵A，和矩阵B；

--- fourplinitialvalue.cpp
+++ fourplinitialvalue.cpp
@@ -1,6 +1,7 @@
 #include "fourplinitialvalue.h"
 #include <cmath>
 #include <algorithm>
+#include <Eigen/Core>
 using namespace std;
 QVector<float> FourPLInitialValue::getReadValue() const
 {
@@ -46,7 +47,7 @@ QVector<float> FourPLInitialValue::getVariationY() const
 {
     return variationY;
 }
-
+//求线性变化后y的值
 void FourPLInitialValue::setVariationY(const QVector<float> &value, float initialA, float initialD)
 {
     QVector<float> tmp1;
@@ -70,10 +71,82 @@ QVector<float> FourPLInitialValue::getVariationX() const
 {
     return variationX;
 }
-
+//求线性变换后想x的值
 void FourPLInitialValue::setVariationX(const QVector<float> &value)
 {
-    variationX = value;
+    QVector<float> tmp;
+    for(int i = 0 ; i < value.size(); i++)
+    {
+        tmp.push_back(log(value.at(i)));
+    }
+    variationX = tmp;
+}
+
+float FourPLInitialValue::getInitialB() const
+{
+    return initialB;
+}
+
+void FourPLInitialValue::setInitialB(float value)
+{
+    initialB = value;
+}
+
+float FourPLInitialValue::getInitialC() const
+{
+    return initialC;
+}
+
+void FourPLInitialValue::setInitialC(float value)
+{
+    initialC = value;
+}
+
+Eigen::Matrix<float, 2, 2> FourPLInitialValue::getA() const
+{
+    return a;
+}
+
+void FourPLInitialValue::setA(QVector<float> X)
+{
+    QVector<float> X2;
+    for(int i = 0; i < X.size(); i++){
+        X2.push_back(pow(X.at(i),2));
+    }
+    float sumX2 = 0.0, sumX = 0.0;
+    for(int i = 0; i < X.size(); i++){
+        sumX2 += X2.at(i);
+        sumX += X.at(i);
+    }
+    Eigen::Matrix<float, 2, 2> tmp;
+    tmp(0,0) = sumX;
+    tmp(0,1) = X.size();
+    tmp(1,0) = sumX2;
+    tmp(1,1) = sumX;
+
+    a = tmp;
+}
+
+Eigen::Matrix<float, 2, 1> FourPLInitialValue::getB() const
+{
+    return b;
+}
+
+void FourPLInitialValue::setB(QVector<float> X, QVector<float> Y)
+{
+    QVector<float> XY;
+    for(int i = 0; i < X.size(); i ++){
+        XY.push_back(X.at(i)*Y.at(i));
+    }
+    float sumY = 0.0, sumXY = 0.0;
+    for(int i = 0; i < Y.size(); i ++){
+        sumY += Y.at(i);
+        sumXY += XY.at(i);
+    }
+    Eigen::Matrix<float, 2, 1> tmp;
+    tmp(0, 0) = -sumY;
+    tmp(0 ,1) = sumXY;
+    b = tmp;
 }
 
 FourPLInitialValue::FourPLInitialValue()
--- fourplinitialvalue.h
+++ fourplinitialvalue.h
@@ -1,10 +1,13 @@
 #ifndef FOURPLINITIALVALUE_H
 #define FOURPLINITIALVALUE_H
 #include <QVector>
+#include <Eigen/Core>
 
 class FourPLInitialValue
 {
 private:
+    Eigen::Matrix<float, 2, 2> a;
+    Eigen::Matrix<float, 2, 1> b;
     float initialA, initialB, initialC, initialD;
     QVector<float> variationY, variationX;
     QVector<float> readValue, dilutionDrugValue;
@@ -20,6 +23,18 @@ public:
     void setInitialA(QVector<float> value);
     float getInitialD() const;
     void setInitialD(QVector<float> value);
+    QVector<float> getVariationY() const;
+    void setVariationY(const QVector<float> &value, float initialA, float initialB);
+    QVector<float> getVariationX() const;
+    void setVariationX(const QVector<float> &value);
+    float getInitialB() const;
+    void setInitialB(float value);
+    float getInitialC() const;
+    void setInitialC(float value);
+    Eigen::Matrix<float, 2, 2> getA() const;
+    void setA(QVector<float> X);
+    Eigen::Matrix<float, 2, 1> getB() const;
+    void setB(QVector<float> X, QVector<float> Y);
 };
 
 #endif // FOURPLINITIALVALUE_H
